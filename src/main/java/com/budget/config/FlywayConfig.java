6c9a6fe06453fdc0fde6e6cb3a1ccf7f89f1a8ac
xdeveloper26-admin <xdeveloper26@outlook.com>
2025-02-08 22:23:43 +0800

    Initial commit: Configure database connection

---
+++ src/main/java/com/budget/config/FlywayConfig.java
@@ -0,0 +1,32 @@
+package com.budget.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class FlywayConfig {
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/budgetapp");
+        dataSource.setUsername("budgetapp");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+
+    @Bean
+    public Flyway flyway(DataSource dataSource) {
+        FluentConfiguration configuration = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:/flyway/migrations")
+                .schemas("budget");
+
+        return new Flyway(configuration);
+    }
+}
