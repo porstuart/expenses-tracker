84d6b56455486bb9c5aa9ea24a2d1b94b0064bc4
Stuart Por <porstuart@outlook.com>
2025-05-09 19:26:12 +0800

    Resolved codacy issue - 41bfc5f

--- src/test/java/com/budget/BudgetApplicationTests.java
+++ src/test/java/com/budget/BudgetApplicationTests.java
@@ -1,5 +1,7 @@
 package com.budget;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class BudgetApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail();
 	}
 
 }
